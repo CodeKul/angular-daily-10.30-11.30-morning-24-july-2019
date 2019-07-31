5262232eeb95c08bb362acee3843f15a969018ed
aniruddha <aniruddha.kudalkar@gmail.com>
2019-07-31 14:56:24 +0530

    directives : structural directives

--- angular/codekul-app/projects/directives/src/app/unless.directive.ts
+++ angular/codekul-app/projects/directives/src/app/unless.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, TemplateRef, Input, OnInit, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appUnless]'
 })
-export class UnlessDirective {
+export class UnlessDirective implements OnInit {
 
-  constructor() { }
+  @Input('appUnless')
+  isSh: boolean
 
+  constructor(
+    private tmRf: TemplateRef<any>,
+    private vcRf: ViewContainerRef
+  ) {
+
+  }
+
+  ngOnInit() {
+
+    if (!this.isSh) {
+      this.vcRf.createEmbeddedView(this.tmRf)
+    } else {
+      this.vcRf.clear()
+    }
+  }
 }
