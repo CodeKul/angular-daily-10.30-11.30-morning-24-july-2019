3c78c1146d8f5538a930dd8f975c856f97ec0f39
aniruddha <aniruddha.kudalkar@gmail.com>
2019-07-31 12:05:21 +0530

    directives : structural directives

---
+++ angular/codekul-app/projects/directives/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directives';
+
+  isJmb = false
+
+  langs = [
+    'java',
+    'cpp',
+    'python',
+    'js',
+    'ts',
+    'c'
+  ]
+
+  clk() {
+    this.isJmb = !this.isJmb
+  }
+}
