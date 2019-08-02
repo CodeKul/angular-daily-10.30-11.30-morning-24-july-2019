2218974708438566f245b8543d7331e00bb77aa6
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-02 12:49:27 +0530

    router : router navigation

--- angular/codekul-app/projects/routing/src/app/login.component.ts
+++ angular/codekul-app/projects/routing/src/app/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router : Router
+  ) { }
 
   ngOnInit() {
   }
 
+  onLogin(usNm: string, pass: string) {
+    this.router.navigate(['dash'])
+  }
 }
--- angular/codekul-app/projects/routing/src/app/login/login.module.ts
+++ angular/codekul-app/projects/routing/src/app/login/login.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './../app-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../login.component';
@@ -5,7 +6,8 @@ import { LoginComponent } from '../login.component';
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class LoginModule { }
